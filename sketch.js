b8ee6773fff008e9e7b299b4047ebb97b2a7136f
sneha375 <demo98551@gmail.com>
2021-04-27 16:19:27 +0530

    khushi

--- sketch.js
+++ sketch.js
@@ -34,7 +34,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+    bird2=new Bird(200,100);
 }
 
 function draw(){
@@ -59,4 +59,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    bird2.display();
 }
